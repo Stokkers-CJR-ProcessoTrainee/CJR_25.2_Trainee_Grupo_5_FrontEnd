17d379fd7bc24e47bd8883869261232d4dd8b9f9
felipeab-b <felipeabferreira22@gmail.com>
2025-10-26 15:55:29 -0300

    feat: criação do cliente HTTP de auth

--- frontend/api/auth.js
+++ frontend/api/auth.js
@@ -1,1 +1,19 @@
 import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export async function login(email, password) {
+  try {
+    const res = await api.post('/login', { email, password });
+    return res.data;
+  } catch (err) {
+    throw err?.response?.data || err;
+  }
+}
+
+export default api;
