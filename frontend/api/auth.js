dcef40d8334f59e83db59f8f2fab97a803a7980c
felipeab-b <felipeabferreira22@gmail.com>
2025-10-26 17:35:00 -0300

    feat: integração do login

--- frontend/api/auth.js
+++ frontend/api/auth.js
@@ -1,19 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:3001',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export async function login(email, password) {
-  try {
     const res = await api.post('/login', { email, password });
     return res.data;
-  } catch (err) {
-    throw err?.response?.data || err;
-  }
 }
-
-export default api;
