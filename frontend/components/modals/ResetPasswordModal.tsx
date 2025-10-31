58959913f04e1cbafced93ed0b28b41283e57b98
felipeab-b <felipeabferreira22@gmail.com>
2025-10-31 16:49:44 -0300

    feat: fuction do modal com as consts

--- frontend/components/modals/ResetPasswordModal.tsx
+++ frontend/components/modals/ResetPasswordModal.tsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+
 interface ResetPasswordModalProps {
   isOpen: boolean;
   onClose: () => void;
   email: string;
+}
+
+export default function ResetPasswordModal({ isOpen, onClose, email }: ResetPasswordModalProps) {
+  const [code, setCode] = useState("");
+  const [codeVerified, setCodeVerified] = useState(false);
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  return null;
 }
