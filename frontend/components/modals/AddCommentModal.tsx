9b3706e898a6a572e32af795d2e7130f57a4c526
felipeab-b <felipeabferreira22@gmail.com>
2025-11-04 09:20:53 -0300

    feat: card do modal e botão de fechar

--- frontend/components/modals/AddCommentModal.tsx
+++ frontend/components/modals/AddCommentModal.tsx
@@ -1,3 +1,5 @@
+import { FaTimes } from "react-icons/fa";
+
 interface AddCommentModalProps {
     mostrar: boolean,
     fechar: () => void,
@@ -9,6 +11,15 @@ export default function AddCommentModal({mostrar, fechar}: AddCommentModalProps)
     if (!mostrar) return null
 
     return(
-        <h1>TESTE</h1>
+        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+            <div className="bg-card rounded-lg p-8 max-w-md w-full text-center shadow-lg relative">
+                <button
+                onClick={() => fechar()}
+                className="absolute top-4 cursor-pointer right-4 text-gray-500 hover:text-gray-800 transition text-2xl"
+                >
+                    <FaTimes />
+                </button>
+            </div>
+        </div>
     );
 }
