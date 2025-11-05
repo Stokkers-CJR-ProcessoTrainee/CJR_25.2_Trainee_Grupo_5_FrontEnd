d69866a0c17e1b9a67f6a0caa2730a1c2cfb68fc
felipeab-b <felipeabferreira22@gmail.com>
2025-11-04 23:19:06 -0300

    feat: div colorida de cima e nova cor de laranja

--- frontend/app/rating/page.tsx
+++ frontend/app/rating/page.tsx
@@ -3,8 +3,12 @@ import Navbar from "@/components/Navbar";
 
 export default function RatingsPage() {
     return(
-        <main>
+        <main className="w-screen min-h-screen flex flex-col">
             <Navbar />
+            <div className="bg-laranja2 w-full h-115">
+
+            </div>
+            <div></div>
         </main>
     );
 }
