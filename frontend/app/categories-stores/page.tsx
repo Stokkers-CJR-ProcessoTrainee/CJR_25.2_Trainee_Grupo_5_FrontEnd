bcb5188700ef8229ad6f89374eb2e86a92390a9a
markvncs <vmarcu090@gmail.com>
2025-12-02 22:28:29 -0300

    feat: começando a estrutura da pagina de categorias de lojas

---
+++ frontend/app/categories-stores/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Navbar from "@/components/Navbar";
+
+export default function CategoriesStoresPage() {
+
+return (
+    <main className="bg-back min-h-screen">
+
+        <Navbar />
+
+        <div className="bg-laranja h-100 w-full">
+
+        </div>
+
+    </main>
+)
+}
